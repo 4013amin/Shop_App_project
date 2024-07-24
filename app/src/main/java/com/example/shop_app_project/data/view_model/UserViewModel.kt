d3275e55353dd789b745307d6403295b56e56983
delta <amin138400138400@gmail.com>
2024-07-24 20:43:30 +0330

    Amin

--- app/src/main/java/com/example/shop_app_project/data/view_model/UserViewModel.kt
+++ app/src/main/java/com/example/shop_app_project/data/view_model/UserViewModel.kt
@@ -9,7 +9,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.shop_app_project.data.models.Profile.Profile
 import com.example.shop_app_project.data.models.product.Category
 import com.example.shop_app_project.data.models.product.PorductModel
 import com.example.shop_app_project.data.models.register.login_model
@@ -24,8 +23,9 @@ class UserViewModel(application: Application) : AndroidViewModel(application) {
     var login_result = mutableStateOf("")
     var products = mutableStateOf<List<PorductModel>>(arrayListOf())
     var category = mutableStateOf<List<Category>>(arrayListOf())
-    var profile = mutableStateOf<List<Profile>>(arrayListOf())
 
+
+    //chech_for_login
     var isLoggedIn by mutableStateOf(false)
 
     private val shoppingCartViewModel = ShoppingCartViewModel(application)
@@ -33,24 +33,10 @@ class UserViewModel(application: Application) : AndroidViewModel(application) {
     private val sharedPreferences =
         application.getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
 
-<<<<<<< HEAD
-    fun sendRegister(
-        username: String,
-        password: String,
-        phone: String,
-        city: String,
-        address: String,
-        postalCode: String,
-    ) {
-        viewModelScope.launch(Dispatchers.IO) {
-            val response = try {
-                Utils_ret.api.registerUser(username, password, phone, city, address, postalCode)
-=======
     fun sendRegister(username: String, password: String, phone: String, location: String) {
         viewModelScope.launch(Dispatchers.IO) {
             val response = try {
                 Utils_ret.api.registerUser(username, password, phone, location)
->>>>>>> UiMainPage
             } catch (e: IOException) {
                 Log.e("UserViewModel", "Network error occurred during registration.", e)
                 registrationResult.value = "Network error occurred."
@@ -70,13 +56,7 @@ class UserViewModel(application: Application) : AndroidViewModel(application) {
                 editor.putString("username", username)
                 editor.putString("password", password)
                 editor.putString("phone", phone)
-<<<<<<< HEAD
-                editor.putString("city", city)
-                editor.putString("address", address)
-                editor.putString("postalCode", postalCode)
-=======
                 editor.putString("location", location)
->>>>>>> UiMainPage
                 editor.apply()
 
             } else {
@@ -85,35 +65,6 @@ class UserViewModel(application: Application) : AndroidViewModel(application) {
         }
     }
 
-<<<<<<< HEAD
-//    fun sendLogin(username: String, address: String) {
-//        viewModelScope.launch(Dispatchers.IO) {
-//            val response = try {
-//                val user = login_model(
-//                    username = username,
-//                    address = address,
-//                )
-//                Utils_ret.api.loginUser(user)
-//            } catch (e: IOException) {
-//                Log.e("UserViewModel", "Network error occurred during login.", e)
-//                login_result.value = "Network error occurred."
-//                return@launch
-//            } catch (e: HttpException) {
-//                Log.e("UserViewModel", "HTTP error occurred during login: ${e.code()}", e)
-//                login_result.value = "HTTP error occurred: ${e.code()}"
-//                return@launch
-//            }
-//
-//            if (response.isSuccessful && response.body() != null) {
-//                Log.d("UserViewModel", "Login successful.")
-//                registrationResult.value = "Login successful."
-//
-//            } else {
-//                Log.e("UserViewModel", "Login failed: ${response.errorBody()}")
-//            }
-//        }
-//    }
-=======
     //checkLogin
     fun checkCredentials(): Boolean {
         val savedCredentials = getSavedCredentials()
@@ -126,7 +77,7 @@ class UserViewModel(application: Application) : AndroidViewModel(application) {
             val response = try {
                 val user = login_model(
                     username = username,
-                    address = address,
+                    address = address, "", "", "", "",
                 )
                 Utils_ret.api.loginUser(user)
             } catch (e: IOException) {
@@ -148,7 +99,6 @@ class UserViewModel(application: Application) : AndroidViewModel(application) {
             }
         }
     }
->>>>>>> UiMainPage
 
     fun getAllProducts() {
         viewModelScope.launch(Dispatchers.IO) {
@@ -207,25 +157,6 @@ class UserViewModel(application: Application) : AndroidViewModel(application) {
         }
     }
 
-    //Profile
-    fun getProfile() {
-        viewModelScope.launch(Dispatchers.IO) {
-            val response = try {
-                Utils_ret.api.getProfile()
-            } catch (e: IOException) {
-                Log.e("getProfileErrorIO", e.toString())
-                return@launch
-            } catch (e: HttpException) {
-                Log.e("getProfileErrorHTTP", e.toString())
-                return@launch
-            }
-
-            if (response.isSuccessful && response.body() != null) {
-                profile.value = response.body()!!
-            }
-        }
-    }
-
 
     fun saveCredentials(username: String, password: String, phone: String, location: String) {
         with(sharedPreferences.edit()) {
@@ -246,20 +177,6 @@ class UserViewModel(application: Application) : AndroidViewModel(application) {
         return Quadruple(username, password, phone, location)
     }
 
-<<<<<<< HEAD
-
-    fun addToCart(product: PorductModel) {
-        shoppingCartViewModel.addToCart(product)
-    }
-
-
-    fun removeFromCart(product: PorductModel) {
-        shoppingCartViewModel.removeFromCart(product)
-    }
-
-
-    fun getCartItems(): List<PorductModel> {
-=======
     data class Quadruple<out A, out B, out C, out D>(
         val first: A,
         val second: B,
@@ -279,10 +196,10 @@ class UserViewModel(application: Application) : AndroidViewModel(application) {
 
     // تابع برای دریافت لیست محصولات در سبد خرید
     fun getCartItems(): List<com.example.shop_app_project.Home_page.Main.ProductModel> {
->>>>>>> UiMainPage
         return shoppingCartViewModel.getCartItems()
     }
 
+    // تابع برای پاک کردن سبد خرید
     fun clearCart() {
         shoppingCartViewModel.clearCart()
     }
