c778460191ee2897941a4b52d3194aa2a731bd44
delta <amin138400138400@gmail.com>
2025-05-28 11:35:13 +0330

    Edit in ChatScreen.kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,9 @@ android {
         applicationId = "com.example.shop_app_project"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
+
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
